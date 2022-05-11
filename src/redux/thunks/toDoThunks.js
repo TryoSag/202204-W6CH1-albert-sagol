83531b33077fe374fd78617ecd0b5d88d074c99f
Albert <albert.sagol@gmail.com>
2022-05-11 08:16:29 +0200

    added edit thunk and fixed load

--- src/redux/thunks/toDoThunks.js
+++ src/redux/thunks/toDoThunks.js
@@ -1,17 +1,32 @@
 import axios from "axios";
 import {
   loadListActionCreator,
+  markAsDoneActionCreator,
   removeItemActionCreator,
 } from "../features/listSlice";
 
 export const loadListThunk = () => async (dispatch) => {
-  const toDos = await axios.get("https://to-do-list-api.onrender.com/todo/");
+  const { data: toDos } = await axios.get(
+    "https://to-do-list-api.onrender.com/todo/"
+  );
 
   dispatch(loadListActionCreator(toDos));
 };
 
 export const removeItemThunk = (id) => async (dispatch) => {
-  await axios.delete("https://to-do-list-api.onrender.com/todo/");
+  await axios.delete(`https://to-do-list-api.onrender.com/todo/${id}`);
 
   dispatch(removeItemActionCreator(id));
 };
+
+export const toggleItemDoneThunk =
+  ({ id, name, done }) =>
+  async (dispatch) => {
+    await axios.put(`https://to-do-list-api.onrender.com/todo/${id}`, {
+      id,
+      name,
+      done: !done,
+    });
+
+    dispatch(markAsDoneActionCreator(id));
+  };
